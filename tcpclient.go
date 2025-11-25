79febde82a9def4dd0778ab36b28543e379424c3
punk-one <punk-player@outlook.com>
2025-11-25 23:01:55 +0800

    fix platform error

--- tcpclient.go
+++ tcpclient.go
@@ -12,7 +12,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 )
 
@@ -112,44 +111,6 @@ type tcpTransporter struct {
 	LastPDUType                   byte
 
 	PDULength int
-}
-
-func setTCPOptions(tcpConn *net.TCPConn) {
-	// 创建TCP选项数据
-	// 创建TCP选项数据
-	options := []byte{
-		0x01,       // NOP
-		0x01,       // NOP
-		0x08, 0x0a, // 时间戳选项长度 + 类型
-		0x00, 0x00, 0x00, 0x00, // 时间戳值预留空间
-		0x00, 0x00, 0x00, 0x00, // 回声时间戳值预留空间
-	}
-	// 获取当前时间戳
-	now := time.Now().UnixNano() / 1000000 // 转换成毫秒
-	timestamp := uint32(now)
-	// 填充时间戳值
-	copy(options[4:], []byte{
-		byte(timestamp >> 24),
-		byte(timestamp >> 16),
-		byte(timestamp >> 8),
-		byte(timestamp),
-	})
-	// 设置TCP选项
-	// 获取连接的文件描述符
-	fd, err := tcpConn.File()
-	if err != nil {
-		fmt.Println("Error getting file:", err)
-		return
-	}
-	defer fd.Close()
-	// 使用 uintptr 转换为 int
-	fdHandle := syscall.Handle(fd.Fd())
-	// 设置 TCP 保活选项
-	syscall.SetsockoptInt(fdHandle, syscall.IPPROTO_TCP, 1, 1) // 设置开始 Keepalive 的时间
-	syscall.SetsockoptInt(fdHandle, syscall.IPPROTO_TCP, 1, 1) // 设置开始 Keepalive 的时间
-	syscall.Setsockopt(fdHandle, syscall.IPPROTO_TCP, 8, &options[4], 10)
-	syscall.Setsockopt(fdHandle, syscall.SOCK_RAW, 8, &options[4], 10)
-	return
 }
 
 func (mb *tcpTransporter) setConnectionParameters(address string, localTSAP uint16, remoteTSAP uint16) {
@@ -417,7 +378,7 @@ func (mb *tcpTransporter) closeIdle() {
 	if mb.IdleTimeout <= 0 {
 		return
 	}
-	idle := time.Now().Sub(mb.lastActivity)
+	idle := time.Since(mb.lastActivity)
 	if idle >= mb.IdleTimeout {
 		mb.logf("s7: closing connection due to idle timeout: %v", idle)
 		mb.close()
