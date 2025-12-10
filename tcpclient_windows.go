a8deeeeb454c56ee131090cdd4efa2837b7b7fdd
punk-one <punk-player@outlook.com>
2025-12-10 17:07:39 +0800

    fix windows connect error

--- tcpclient_windows.go
+++ tcpclient_windows.go
@@ -7,13 +7,15 @@ package gos7
 // This software may be modified and distributed under the terms
 // of the BSD license. See the LICENSE file for details.
 import (
-	"fmt"
 	"net"
 	"syscall"
 	"time"
 )
 
 func setTCPOptions(tcpConn *net.TCPConn) {
+	// 在Windows上，使用标准库的SetKeepAlive方法
+	_ = tcpConn.SetKeepAlive(true)
+
 	// 创建TCP选项数据
 	options := []byte{
 		0x01,       // NOP
@@ -32,19 +34,22 @@ func setTCPOptions(tcpConn *net.TCPConn) {
 		byte(timestamp >> 8),
 		byte(timestamp),
 	})
-	// 设置TCP选项
-	// 获取连接的文件描述符
-	fd, err := tcpConn.File()
+
+	// 在Windows上，使用SyscallConn方法获取底层的socket句柄
+	// 这是Go 1.11+引入的方法，可以在Windows上安全地获取socket句柄
+	rawConn, err := tcpConn.SyscallConn()
 	if err != nil {
-		fmt.Println("Error getting file:", err)
+		// 如果SyscallConn失败，只使用标准库的SetKeepAlive
 		return
 	}
-	defer fd.Close()
-	// 使用 uintptr 转换为 Windows Handle
-	fdHandle := syscall.Handle(fd.Fd())
-	// 设置 TCP 保活选项
-	syscall.SetsockoptInt(fdHandle, syscall.IPPROTO_TCP, 1, 1) // 设置开始 Keepalive 的时间
-	syscall.SetsockoptInt(fdHandle, syscall.IPPROTO_TCP, 1, 1) // 设置开始 Keepalive 的时间
-	syscall.Setsockopt(fdHandle, syscall.IPPROTO_TCP, 8, &options[4], 10)
-	syscall.Setsockopt(fdHandle, syscall.SOCK_RAW, 8, &options[4], 10)
+
+	// 使用Control方法访问底层的socket句柄
+	rawConn.Control(func(fd uintptr) {
+		fdHandle := syscall.Handle(fd)
+		// 设置 TCP_NODELAY 选项
+		_ = syscall.SetsockoptInt(fdHandle, syscall.IPPROTO_TCP, syscall.TCP_NODELAY, 1)
+		// 尝试设置TCP时间戳选项（如果支持）
+		// 注意：TCP时间戳选项在某些Windows版本上可能不支持
+		_ = syscall.Setsockopt(fdHandle, syscall.IPPROTO_TCP, 8, &options[4], 10)
+	})
 }
